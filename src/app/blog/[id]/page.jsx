60623b64b08a0229ca7b98a5e2482c940c5fff5c
chikusungh65 <110668294+chikusungh65@users.noreply.github.com>
2023-07-21 19:16:38 +0530

    added about , contact , blog , home pages.

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,10 +1,47 @@
 import React from 'react'
 import styles from './page.module.css'
-
+import Image from 'next/image';
 const BlogPost = () => {
     return (
-        <div className={styles.container}>BlogPost</div>
-    )
-}
-
+        <div className={styles.container}>
+            <div className={styles.top}>
+                <div className={styles.info}>
+                    <h1 className={styles.title}>
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga, commodi.
+                    </h1>
+                    <p className={styles.desc}>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae sequi nihil quasi nam sunt corrupti ad numquam magni cupiditate doloribus!
+                    </p>
+                    <div className={styles.author}>
+                        <Image
+                            src="https://images.pexels.com/photos/3194521/pexels-photo-3194521.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                            alt=""
+                            width={40}
+                            height={40}
+                            className={styles.avatar}
+                        />
+                        <span className={styles.username}>Ankit Kumar</span>
+                    </div>
+                </div>
+                <div className={styles.imageContainer}>
+                    <Image
+                        src="https://images.pexels.com/photos/3194521/pexels-photo-3194521.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                        alt=""
+                        fill={true}
+                        className={styles.image}
+                    />
+                </div>
+            </div>
+            <div className={styles.content}>
+                <p className={styles.text}>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus pariatur facilis totam, aliquid, perspiciatis incidunt perferendis ipsa molestias, nisi consequuntur maxime! Voluptas facilis beatae ullam molestias. Id non qui numquam inventore quidem sequi tenetur optio neque, ratione cupiditate eveniet magni?
+                    <br />
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quod nihil vero distinctio sapiente hic sequi asperiores et, qui tenetur ab omnis animi! Impedit ducimus quia esse excepturi vel. Odio, consequatur?
+                    <br />
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi sed voluptate repellendus cumque repudiandae provident vitae aspernatur rem ducimus, aperiam explicabo a laborum. Quae ducimus similique culpa commodi? Obcaecati architecto alias eveniet ipsum commodi eaque voluptates qui tempora ducimus. Officiis, voluptate. Laborum fugit placeat velit eligendi, atque similique voluptates error? Reiciendis pariatur aliquam quisquam ut delectus ex praesentium optio sequi?
+                </p>
+            </div>
+        </div>
+    );
+};
 export default BlogPost
--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
 import styles from './page.module.css'
-const Category = () => {
+import Image from 'next/image'
+import Button from '@/components/Button/Button'
+const Category = ({ params }) => {
+
     return (
-        <div className={styles.container}>Category</div>
+        <div className={styles.container}>
+            <h1 className={styles.catTitle}>
+                {params.category}
+            </h1>
+            <div className={styles.item}>
+                <div className={styles.content}>
+                    <h1 className={styles.title}>Test</h1>
+                    <p className={styles.desc}>Desc</p>
+                    <Button text="See More" url="#" />
+                </div>
+                <div className={styles.imgContainer}>
+                    <Image
+                        src="https://images.pexels.com/photos/3194521/pexels-photo-3194521.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                        fill={true}
+                        alt=""
+                        className={styles.img}
+                    />
+                </div>
+            </div>
+        </div>
     )
 }
 
